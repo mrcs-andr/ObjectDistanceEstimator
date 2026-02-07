54df68f80eb7ca22633be8b5b2a6c2c6ca312566
Marcos Andrade <mrcs.andr@gmail.com>
2026-02-07 17:11:55 -0300

    Adding Intrinsics calibration

---
+++ app/src/main/java/com/mrcs/andr/objectdistanceestimatorapp/IntrinsicsCalibrationActivity.java
@@ -0,0 +1,15 @@
+package com.mrcs.andr.objectdistanceestimatorapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntrinsicsCalibrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intrinsics_calibration);
+    }
+
+}
